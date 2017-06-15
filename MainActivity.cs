c480af6e1b0f1f59916cfdbd73d2d1063687c9a2
강준 <junijuniya@naver.com>
2017-06-15 23:09:19 +0900

    초기 커밋.

---
+++ MainActivity.cs
@@ -0,0 +1,34 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Gms.Ads;
+
+namespace AdMob
+{
+    [Activity(Label = "AdMob", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected AdView adView;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView (Resource.Layout.Main);
+            adView = FindViewById<AdView>(Resource.Id.adView);
+            var adRequest = new AdRequest.Builder().Build();
+            adView.LoadAd(adRequest);
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            if (adView != null)
+            {
+                adView.Resume();
+            }
+        }
+    }
+}
+
